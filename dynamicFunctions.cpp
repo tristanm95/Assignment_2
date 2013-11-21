d2109a9d24cfc46c84ab4af882793fd295c204c9
Tristan May <tristanm@sfu.ca>
2013-11-21 14:35:43 -0800

    Added more of the dynamic functions
    Well this is more difficult than I imagined...

--- dynamicFunctions.cpp
+++ dynamicFunctions.cpp
@@ -9,10 +9,6 @@ bool MakeMove(int** connectNBoard, int numRowsInBoard,  int player, int columnCh
 bool DisplayBoard( int** connectNBoard,  int numRowsInBoard);
 bool CheckWinner( int** connectNBoard, int numRowsInBoard, int numConnect, int columnChosen, int player );
 
-int main()
-{
-	return 0;
-}
 
 bool InitializeBoard(int** connectNBoard,  int numRowsInBoard)
 {
@@ -25,14 +21,14 @@ bool InitializeBoard(int** connectNBoard,  int numRowsInBoard)
 	connectNBoard = new (nothrow) int[numRowsInBoard];
 	if(connectNBoard == NULL)
 	{
-		return(false);
+		return false;
 	}
 
 	for(index = 0; index < numRowsInBoard; index++)
 	{
 		*(connectNBoard + index) = 0;
 	}
-	return(true);
+	return true;
 }
 
 bool MakeMove(int** connectNBoard, int numRowsInBoard,  int playeID, int columnChosen)
@@ -55,18 +51,18 @@ bool MakeMove(int** connectNBoard, int numRowsInBoard,  int playeID, int columnC
 	if(columnChosen > numRowsInBoard || columnChosen < 0)
 	{
 		cout << "Illegal move";
-		return(false);
+		return false;
 	}
 	
 	else if(*(connectNBoard + columnChosen) != 0)
 	{
 		cout << "Illegal move";
-		return(false);
+		return false;
 	}
 
 	else
 	{
-		cout << "Something went wrong";
+		NULL;
 	}
 
 	for(index = 0; index < numRowsInBoard; index++)
@@ -83,7 +79,7 @@ bool MakeMove(int** connectNBoard, int numRowsInBoard,  int playeID, int columnC
 		
 	}
 	cout << playerName << " has moved";
-	return(true);
+	return true;
 }
 
 bool DisplayBoard( int** connectNBoard,  int numRowsInBoard)
@@ -108,5 +104,68 @@ bool DisplayBoard( int** connectNBoard,  int numRowsInBoard)
                         cout << endl;
                 }
         }
-        return(true);
+        return true;
+}
+
+bool CheckWinner(int** connectNBoard, int numRowsInBoard, int numConnect, int columnChosen, int player)
+{
+	int afterCount = 0;
+	int beforeCount = 0;
+	int vertCount = 0;
+	int index = 0;
+	//Add errors below this comment for invalid columns chosen
+	//
+	//
+	
+	//Checks to the left of the column chosen
+	for(index = numConnect - 1; index >= 0; index--)
+	{
+		if(*((connectNBoard + columnChosen) * index) != 0)
+		{
+			beforeCount += 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+	
+	//Checks to the right of the column chosen
+	for(index = 0; index < numConnect; index++)
+	{
+		if(*((connectNBoard + columnChosen) * index) != 0)
+		{
+			afterCount += 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+	
+	//Checks to see if the player has won by row
+	if(beforeCount + afterCount + 1 >= numConnect)
+	{
+		return true;
+	}
+	
+	//Checks below the column to see if 
+	for(index = numConnect - 1; index >= 0; index--)
+	{
+		if(*(connectNBoard + index) != 0)
+		{
+			vertCount += 0;
+		}
+		else
+		{
+			break;
+		}
+	}
+	//Checks to see if the player has won by column
+	if(vertCount + 1 >= numConnect)
+	{
+		return true;
+	}
+	
+	//Another function will go here to check the diagnoal parts
 }
