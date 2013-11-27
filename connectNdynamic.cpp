b0d14f8d4228aef4992ce783525ede23a68c04d6
Tristan May <tristanm@sfu.ca>
2013-11-27 11:15:58 -0800

    Function Functionality to Dynamic version of the program
    Added in the functions that will be called. Actual functions are located under dynamicFunctions.cpp and will be copied over after being bugtested.

--- connectNdynamic.cpp
+++ connectNdynamic.cpp
@@ -5,12 +5,93 @@
 
 include namespace std;
 
-bool InitializeBoard(int** connectNBoard,  int numRowsInBoard );
-bool MakeMove(int** connectNBoard, int numRowsInBoard,  int player, int columnChosen);
-bool DisplayBoard( int** connectNBoard,  int numRowsInBoard);
-bool CheckWinner( int** connectNBoard, int numRowsInBoard, int numConnect, int columnChosen, int player );
+bool InitializeBoard(int **connectNBoard,  int numRowsInBoard );
+bool MakeMove(int **connectNBoard, int numRowsInBoard,  int player, int columnChosen);
+bool DisplayBoard(int **connectNBoard,  int numRowsInBoard);
+bool CheckWinner(int **connectNBoard, int numRowsInBoard, int numConnect, int columnChosen, int player );
 
 int main()
 {
+	char **connectNBoardpp;
+	int index = 0;
+	int jindex = 0;
+	int numRows = 0;
+	int numConnect = 0;
+	int numToConnect = 0;
+	int playerID = 0;
+	int forfeitIndex = 0;
+	int turnIndex = 0;
+	
+	
+	do
+	{
+		cout << "Please enter the size of the board" << endl;
+		cout << "8 <= numRows <= 25";
+	}
+	while(!(cin >> numRows) || !(8 <= numRows && numRows <= 25));
+	
+	do
+	{
+		cout << "Please enter the number of pieces in a row to win" << endl;
+		cout << "4 <= numToConnect <= numRows - 4";
+	}
+	while(!(cin >> numToConnect) || !(4 <= numToConnect <= (numRows - 4)));
+	
+	connectNBoardpp = new int* [numRows];
+	
+	for(index = 0; index < numRows; index++)
+	{
+		*(connectNBoardpp + index) = new int [numRows];
+	}
+	
+	if(!(InitializeBoard(connecNBoardpp, numRows))
+	{
+		exit(1)
+	}
+	
+	
+	//Put this into a loop
+	for(turnIndex = 0; turnIndex < 99999999999999; turnIndex ++)
+	{
+
+		if(!(MakeMove(connectNBoardpp, numRows, playerID, columnChosen)))
+		{
+			forfeitIndex++;
+			if(forfeitIndex <= 3)
+			{
+				if(playerID == 1)
+				{
+					cout << "Red has forfeited a turn" << endl;
+					continue;
+				}
+				if(playerID == 2)
+				{
+					cout << "Black has forfeited a turn" << endl;
+					continue;
+				}
+			}
+		}
+		
+		if(CheckWinner(connectNBoardpp, numRows, numToConnect, columnChosen, playerID))
+		{
+			if(playerID == 1)
+			{
+				cout << "Red has won";
+				break;
+			}
+			if(playerID == 2)
+			{
+				cout << " Black has won";
+				break;
+			}
+		}
+		
+		if(!(DisplayBoard(connectNBoardpp, numRows)))
+		{
+			cerr << "Could not display board";
+			exit(2);
+		}
+	}
+	
 	return 0;
 }
