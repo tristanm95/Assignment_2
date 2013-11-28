64f3b9f35a814d014b54acb80b62047f29d9e8fb
Tristan May <tristanm@sfu.ca>
2013-11-28 14:08:13 -0800

    Major Change
    Winners function done?

--- dynamicFunctions.cpp
+++ dynamicFunctions.cpp
@@ -104,6 +104,9 @@ bool CheckWinner(int** connectNBoard, int numRowsInBoard, int numConnect, int co
 	int afterCount = 0;
 	int beforeCount = 0;
 	int vertCount = 0;
+	int *placementp = NULL;
+	int *placementX = NULL;
+	int *placementY = NULL;
 	int i = 0;
 	int j = 0;
 	
@@ -126,16 +129,180 @@ bool CheckWinner(int** connectNBoard, int numRowsInBoard, int numConnect, int co
 		}
 	}
 	
+	for(i = 0; i < (numRowsInBoard - 1); i ++)
+	{
+		if(connectNBoard[i][columnChosen] == 'B' && playerID == 1))
+		{
+			placementp = connectNBoard[i][columnChosen];
+			break;
+		}
+		if(connectNBoard[i][columnChosen] == 'R' && playerID == 2))
+		{
+			placementp = connectNBoard[i][columnChosen];
+			break;
+		}
+		
+	}
+	
 	//Horizontal Checker to the right of columnChosen
+	for(i = (placementp + 1); i < (numRowsInBoard - 1); i++)
+	{
+		if(i == 'B' && playerID == 2)
+		{
+			afterCount += 1;
+		}
+		else if(i == 'R' && playerID == 1)
+		{
+			afterCount += 1;
+		}
+		else
+		{
+			break;
+		}
+	}
 	
 	//Horizontal Checker to the left of columnChosen
+	for(i = (placementp - 1); i > 0; i--)
+	{
+		if(i == 'B' && playerID == 2)
+		{
+			beforeCount += 1;
+		}
+		else if(i == 'R' && playerID == 1)
+		{
+			beforeCount += 1;
+		}
+		else
+		{
+			break;
+		}
+	}
 	
+	if((beforeCount + afterCount + 1) >= numConnect)
+	{
+		return true;
+	}
+	
+	for(i = 0; i < numRowsInBoard; i++)
+	{
+		for(j = 0; j < numRowsInBoard; j++)
+		{
+			if(connectNBoard[i][columnChosen] == 'B' && playerID == 1))
+			{
+				placementp = connectNBoard[i][columnChosen];
+				break;
+			}
+			if(connectNBoard[i][columnChosen] == 'R' && playerID == 2))
+			{
+				placementp = connectNBoard[i][columnChosen];
+				break;
+			}
+		}
+	}
 	//Diagonal Checker to the top right of columnChosen
+	do
+	{
+		i = placementp;
+		j = placementp;
+		if(connectNBoard[i+1][j+1] == 'R' && playerID == 2)
+		{
+			afterCount += 1;
+			j++;
+			i++;
+		}
+		else if(connectNBoard[i+1][j+1] == 'B' && playerID == 1)
+		{
+			afterCount += 1;
+			j++;
+			i++;
+		}
+		else
+		{
+			i++;
+			j++;
+			break;
+		}
+	}
+	while(j < numRowsInBoard && i < numRowsInboard);
 	
 	//Diagonal Checker to the bottom left of columnChosen
+	do
+	{
+		if(connectNBoard[i-1][j-1] == 'R' && playerID == 2)
+		{
+			beforeCount += 1;
+			j--;
+			i--;
+		}
+		else if(connectNBoard[i-1][j-1] == 'B' && playerID == 1)
+		{
+			beforeCount += 1;
+			j--;
+			i--;
+		}
+		else
+		{
+			j--;
+			i--;
+			break;
+		}
+	}
+	while(j > 0 && i > 0);
+	
+	if(beforeCount + afterCount + 1 >= numConnect)
+	{
+		return true;
+	}
 	
 	//Diagonal Checker to the bottom right of columnChosen
+	do
+	{
+		if(connectNBoard[i-1][j+1] == 'R' && playerID == 2)
+		{
+			beforeCount += 1;
+			j++;
+			i--;
+		}
+		else if(connectNBoard[i-1][j+1] == 'B' && playerID == 1)
+		{
+			beforeCount += 1;
+			j++;
+			i--;
+		}
+		else
+		{
+
+			break;
+		}
+	}
+	while(j < numRowsInBoard && i > 0);
 	
 	//Diagonal CHecker to the top left of oclumnChosen
+	do
+	{
+		if(connectNBoard[i+1][j-1] == 'R' && playerID == 2)
+		{
+			beforeCount += 1;
+			j--;
+			i++;
+		}
+		else if(connectNBoard[i+1][j-1] == 'B' && playerID == 1)
+		{
+			beforeCount += 1;
+			j--;
+			i++;
+		}
+		else
+		{
+
+			break;
+		}
+	}
+	while(j < numRowsInBoard && i > 0);
+	
+	if((beforeCount + afterCount + 1) >= numConnect)
+	{
+		return true;
+	}
 
 }
