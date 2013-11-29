0be8ab061c971cba591c45f56b3250ce4ba4bcd0
Jason <js2park.psi@gmail.com>
2013-11-29 11:03:18 -0800

    Update connectNstatic.cpp

--- connectNstatic.cpp
+++ connectNstatic.cpp
@@ -30,21 +30,21 @@ int main()
   do
   {
     cout << "Please enter the correct size of the board between " << MIN_ARRAY_SIZE << " <= numRows <= " << MAX_ARRAY_SIZE << " ";
-	cin.clear();
-	cin.sync();
+        cin.clear();
+        cin.sync();
   }while ((!(cin >> numRows)) || ( numRows < MIN_ARRAY_SIZE ) || ( numRows > MAX_ARRAY_SIZE ) );
   
   do
   {
     cout << "Please enter the number of game pieces in a row needed to win the game";
     cin.clear();
-	cin.sync();
+        cin.sync();
     
   }while (  !(cin >> numToConnect)|| !(minRowsToWin <= numToConnect) || !(numToConnect <= numRows - minRowsToWin));
   
   if (!(IntializeBoard(connectNArray,numRows)))
   {
-	exit(1);  
+        exit(1);  
   }
 
   return 0; 
@@ -52,23 +52,103 @@ int main()
 
 //Definitions of staticFunctions
 
-bool MakeMove (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard, int player, int columnChosen)
-{
-  //if the move is not legal, then print a line of text to the screen "Illegal move"
-  return true;
-  //print a line of text tot he screen that says "Red has moved" or "Black has moved"
-}
-bool DisplayBoard (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard)
-{
-  return true;
-}
 bool IntializeBoard(int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard)
 {
+	int i = 0;
+	int j = 0;
 
+	for ( i = 0; i < MAX_ARRAY_SIZE; i++)
+	{
+		for ( j = 0; j < MAX_ARRAY_SIZE; j++)
+		{
+			connectNBoard[i][j] = 0;
 
-  return true;
+			if((connectNBoard[i][j] < 0) || (connectNBoard[i][j] > 2))
+			{
+				cerr << "The board was not initialized";
+				return false;
+			}
+		}
+		
+	}
+	return true;
 }
+
+bool DisplayBoard (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard)
+{
+	int i = 0;
+	int j = 0;
+
+	cout << endl << setw(3) << " ";
+	for (i = 0; i < numRowsInBoard; i++)
+	{
+		cout << setw(3) << i;
+	}
+	cout << endl;
+
+	for (i = 0; i < numRowsInBoard; i++)
+	{
+		cout << left << setw(3) << i;
+		for (j = 0; j < numRowsInBoard; j++)
+		{
+			if ( (j % (numRowsInBoard + 1)) == numRowsInBoard)
+			{
+				cout << endl;
+			}
+			if (connectNBoard[i][j] == 1)//R, 2 = B
+			{
+				cout << setw(3) << right << "R";
+			}else if (connectNBoard[i][j] == 2)
+			{
+				cout << setw(3) << right << "B"; 
+			}else 
+			{
+				cout << setw(3) << right << "o";
+			}
+		}
+	}
+	return true;
+}
+
+bool MakeMove (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard, int player, int columnChosen)
+{
+	//red must go first
+	int i = 0;
+	int j= 0;
+	
+	if (columnChosen < 0 || (columnChosen > numRowsInBaord))
+	{
+		cerr << "Illegal Move";
+		return false;
+	}
+
+	for (i = (numRowsInBoard - 1); i >= 0; i--)
+	{
+		if (connectNBoard[i][columnChosen] == 0)
+		{
+			if (player == 1)
+			{
+				connectNBoard[i][columnChosen] = 1;
+				cout << "Red has moved";
+				return true;
+			}else if (player == 2)
+			{
+				connectNBoard[i][columnChosen] = 2;
+				cout << "Black has moved";
+				return true;
+			}else 
+			{
+				return false;
+			}
+		}
+	}
+		
+	return true;
+}
+
+
 bool CheckWinner (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard, int numConnect, int columnChosen, int playerID)
 {
-  return true;
+
+	return true;
 }
