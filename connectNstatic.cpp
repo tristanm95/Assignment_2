e4580e3b201dfa588ef7eb266e1f1bedbf657d96
candyline <candyline25@gmail.com>
2013-11-28 17:25:10 -0800

    cin loops when letters are entered

--- connectNstatic.cpp
+++ connectNstatic.cpp
@@ -1,19 +1,14 @@
-// Creation date: November 20th, 2013
-// Last modified: November 30th, 2013
-// Authors: Tristian May, Jason Park, Danielle Sinha, Frank Su
-
-
 #include <iostream>
 #include <string>
 #include <cmath>
 #include <iomanip>
 using namespace std;
 
-// Gobal constants, used as min and max size of the game board. 
+//gobal constants, used as min and max size of the game board. 
 const int MAX_ARRAY_SIZE = 25;
 const int MIN_ARRAY_SIZE = 8; 
 
-// Prototypes of the staticFunctions
+//prototypes of the staticFunctions
 
 bool MakeMove (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard, int player, int columnChosen);
 bool DisplayBoard (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard);
@@ -24,54 +19,56 @@ bool CheckWinner (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRows
 int main()
 {
   const int minRowsToWin = 4;
-  // Array set up for connectNboard
-  int connectNArray[MAX_ARAY_SIZE][MAX_ARRAY_SIZE] = {0};
+  //array set up for connectNboard
+  int connectNArray[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE] = {0};
   
-  // Initialize and declare variables
+  //initialize and declare variables
   int numRows = 0;
   int numToConnect = 0;
   
-  // First prompt
-  // Size of board
-  // Number in a row ro win
+  //first prompt
   do
   {
     cout << "Please enter the correct size of the board between " << MIN_ARRAY_SIZE << " <= numRows <= " << MAX_ARRAY_SIZE << " ";
-    cin >> numRows;
-  }while (( numRows < MIN_ARRAY_SIZE ) || ( numRows > MAX_ARRAY_SIZE ));
+	cin.clear();
+	cin.sync();
+  }while ((!(cin >> numRows)) || ( numRows < MIN_ARRAY_SIZE ) || ( numRows > MAX_ARRAY_SIZE ) );
   
   do
   {
     cout << "Please enter the number of game pieces in a row needed to win the game";
-    cin >> numToConnect;
+    cin.clear();
+	cin.sync();
     
-  }while ( !(minRowsToWin <= numToConnect) || !(numToConnect <= numRows - minRowsToWin));
+  }while (  !(cin >> numToConnect)|| !(minRowsToWin <= numToConnect) || !(numToConnect <= numRows - minRowsToWin));
   
-  if (!(InitializeBoard(connectNArray,numRows)));
+  if (!(IntializeBoard(connectNArray,numRows)))
   {
-    exit(1);
+	exit(1);  
   }
-  
+
   return 0; 
 }
 
-// Definitions of staticFunctions
+//Definitions of staticFunctions
 
 bool MakeMove (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard, int player, int columnChosen)
 {
-  // If the move is not legal, then print a line of text to the screen "Illegal move"
-  
-  // Print a line of text tot he screen that says "Red has moved" or "Black has moved"
+  //if the move is not legal, then print a line of text to the screen "Illegal move"
+  return true;
+  //print a line of text tot he screen that says "Red has moved" or "Black has moved"
 }
 bool DisplayBoard (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard)
 {
-  
+  return true;
 }
 bool IntializeBoard(int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard)
 {
-  
+
+
+  return true;
 }
 bool CheckWinner (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard, int numConnect, int columnChosen, int playerID)
 {
-  
+  return true;
 }
