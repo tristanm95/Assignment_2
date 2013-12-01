644795acbff19e5a5e0a7dfd47bf382b13b88372
Tristan May <tristanm@sfu.ca>
2013-11-30 19:24:24 -0800

    Removed copy
    Fixed jason's copying and pasting to both cpp files AGAIN ;)

--- connectNstatic.cpp
+++ connectNstatic.cpp
@@ -1,150 +1,109 @@
-//Initialize Libraries
 #include <iostream>
 #include <iomanip>
-
 using namespace std;
-//This program is for the game called connectN. It is played similarily to connect 4, however the board and the number
-//needed to connect are customizable by the players.
-//
-//
-//Date: 11/29/2013
-//Authors: Tristan May, Frank Su, Jason Park, Danielle Sinha
 
-//Function prototypes for main()
-bool InitializeBoard(int **connectNBoard,  int numRowsInBoard );
-bool MakeMove(int **connectNBoard, int numRowsInBoard,  int player, int columnChosen);
-bool DisplayBoard(int **connectNBoard,  int numRowsInBoard);
-bool CheckWinner(int **connectNBoard, int numRowsInBoard, int numConnect, int columnChosen, int player );
+//gobal constants, used as min and max size of the game board. 
+const int MAX_ARRAY_SIZE = 25;
+const int MIN_ARRAY_SIZE = 8; 
 
-//Start main program
+//prototypes of the staticFunctions
+
+bool MakeMove (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard, int player, int columnChosen);
+bool DisplayBoard (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard);
+bool IntializeBoard(int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard);
+bool CheckWinner (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard, int numConnect, int columnChosen, int playerID);
+
+
 int main()
 {
-	//Initialize dynamic array pointer
-	int **connectNBoardpp;
-	//Initialize counter indexes
+	const int minRowsToWin = 4;
+
+	//array set up for connectNboard
+	int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE] = {0};
+
+	//initialize and declare variables
 	int i = 0;
 	int index = 0;
 	int j = 0;
-	int forfeitIndex = 0;
-	int turnIndex = 0;
-	//Initialize other variables
+
 	int numRows = 0;
 	int numConnect = 0;
 	int numToConnect = 0;
 	int playerID = 0;
+
 	int colChosen = 0;
-	
-	//Request from user the size of the board and repeat if it is out of range or an invalid character
+	int forfeitIndex = 0;
+	int turnIndex = 0;
+	//first prompt
 	do
 	{
 		cout << "Please enter the size of the board" << endl;
-		cout << "8 <= numRows <= 25: ";
-		//Clears the error flag from cin and clears the stream
+		cout << MIN_ARRAY_SIZE << " <= numRows <= " << MAX_ARRAY_SIZE << ": ";
 		cin.clear();
 		cin.sync();
 	}
-	while(!(cin >> numRows) || !(8 <= numRows && numRows <= 25));
-	
-	//Request from user number of pieces a player must have to win
+	while ((!(cin >> numRows)) || ( numRows < MIN_ARRAY_SIZE ) || ( numRows > MAX_ARRAY_SIZE ) );
+
 	do
 	{
 		cout << "Please enter the number of pieces in a row to win" << endl;
-		cout << "4 <= numToConnect <= numRows - 4: ";
-		//Clears the error flag from cin and clears the stream
+		cout << minRowsToWin << " <= numToConnect <= numRows - 4: ";
 		cin.clear();
 		cin.sync();
-	}
-	while(!(cin >> numToConnect) || !(4 <= numToConnect && numToConnect <= (numRows - 4)));
-	
-	//Initializes the pointer to an array of pointers that point to the rows
-	connectNBoardpp = new int* [numRows];
-	
-	//Initializes pointers to arrays of the columns into the array of pointers initialized above
-	for(index = 0; index < numRows; index++)
+
+	}while (  !(cin >> numToConnect)|| !(minRowsToWin <= numToConnect) || !(numToConnect <= numRows - minRowsToWin));
+
+	cout << endl;
+
+	if (!(IntializeBoard(connectNBoard,numRows)))
 	{
-		*(connectNBoardpp + index) = new int [numRows];
+		exit(1);  
 	}
-	
-	//If the board does not initialize, exit the program and delete the arrays
-	if(!(InitializeBoard(connectNBoardpp, numRows)))
-	{	
-		cerr << "The board was not initialized" << endl;
-		for(i = 0; i < numRows; i++)
-		{
-			delete [] connectNBoardpp[i];
-			connectNBoardpp[i] = NULL;
-		}
-		
-		delete [] connectNBoardpp;
-		exit(1);
-	}
-	
-	//Sets the first player to red
-	cout << "Red goes first";
+
+	cout << "Red goes first\n";
 	playerID = 1;
 
-	//Begins the actual game, with a maximum turn value of the total amount of space
-	//in the board
 	for(turnIndex = 0; turnIndex < (numRows * numRows); turnIndex++)
 	{
-		//Clears the stream and any error flag from cin
 		cin.clear();
 		cin.sync();
 		cout << endl;
-		
-		//If the display board function fails, exit the program and delete the arrays
-		if(!(DisplayBoard(connectNBoardpp, numRows)))
+
+		if(!(DisplayBoard(connectNBoard, numRows)))
 		{
 			cerr << "ERROR: COULD NOT DISPLAY BOARD";
-			for(i = 0; i < numRows; i++)
-			{
-				delete [] connectNBoardpp[i];
-				connectNBoardpp[i] = NULL;
-			}
-	
-			delete [] connectNBoardpp;
-			connectNBoardpp = NULL;
-			exit(3);
+			exit (2);
 		}
-		
-		//Request the column that the piece will be placed
-		cout << "Enter the column where you wish to place your piece ";
+
+		cout << "Enter the column number you wish to place piece within ";
 		cin >> colChosen;
 
-		//If the makeMove function returns false, add to the forfeit index,
-		//Clear the stream and cin flag
-		//Request to move again, until the forfeit Index reaches 3
-		for(forfeitIndex = 1; !(MakeMove(connectNBoardpp, numRows, playerID, colChosen)) && forfeitIndex < 3; ++forfeitIndex)
+		for (forfeitIndex = 1;  !(MakeMove(connectNBoard, numRows, playerID, colChosen)) && forfeitIndex < 3; ++forfeitIndex)
 		{
 			cin.clear();
 			cin.sync();
 			cin >> colChosen;
 		}
 
-		//If the forfeit index is >= 3, switch players from Red to Black
-		//Go to the top of the turn loop
 		if(forfeitIndex >= 3 && playerID == 1)
 		{
-			cout << endl << "Red has forfeited their move";
+			cout << endl << endl << "Red has forfeited their move";
 			playerID = 2;
 			forfeitIndex = 0;
 			continue;
 		}
-		//If the forfeit index is >= 3, switch players from Black to Red
-		//Go to the top of the turn loop
+	
 		if(forfeitIndex >= 3 && playerID == 2)
 		{
-			cout << endl << "Black has forfeited their move";
+			cout << endl << endl << "Black has forfeited their move";
 			playerID = 1;
 			forfeitIndex = 0;
 			continue;
 		}
-		//Resets the forfeit index
+
 		forfeitIndex = 0;
 
-		//If the checkWinner function is true, output who has one from the current PlayerID
-		//Break out of the turn function
-		if(CheckWinner(connectNBoardpp, numRows, numToConnect, colChosen, playerID))
+		if(CheckWinner(connectNBoard, numRows, numToConnect, colChosen, playerID))
 		{
 			if(playerID == 1)
 			{
@@ -157,9 +116,9 @@ int main()
 				break;
 			}
 		}
-		
-		//Switch playerID for the next turn
-		if(playerID == 1)
+
+
+		if (playerID == 1)
 		{
 			playerID = 2;
 		}
@@ -168,146 +127,68 @@ int main()
 			playerID = 1;
 		}
 	}
-	
-	//Delete the columns
-	//Set the pointers to the columns to NULL
-	for(i = 0; i < numRows; i++)
-	{
-		delete [] connectNBoardpp[i];
-		connectNBoardpp[i] = NULL;
-	}
-	
-	//Delete the array that contains the pointers that point to the columns
-	delete [] connectNBoardpp;
-	//Sets the pointer that points to the array of pointers that point to the columns to NULL
-	connectNBoardpp = NULL;
-	
-	
-	return 0;
+
+	return 0; 
 }
 
-//This function initializes the game board to 0
-bool InitializeBoard(int** connectNBoard,  int numRowsInBoard)
+//Definitions of staticFunctions
+
+bool IntializeBoard(int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard)
 {
 	int i = 0;
 	int j = 0;
-	
-	//Starts the for loop that will initialize the board
-	for(i = 0; i < numRowsInBoard; i++)
+
+	for ( i = 0; i < MAX_ARRAY_SIZE; i++)
 	{
-		for(j = 0; j < numRowsInBoard; j++)
+		for ( j = 0; j < MAX_ARRAY_SIZE; j++)
 		{
-			//Sets location i,j of the board to 0
 			connectNBoard[i][j] = 0;
-			
-			//if the above statement does not work, output an error message and return false
-			if(connectNBoard[i][j] < 0 || connectNBoard[i][j] > 2)
+
+			if((connectNBoard[i][j] < 0) || (connectNBoard[i][j] > 2))
 			{
+				cerr << "The board was not initialized";
 				return false;
 			}
 		}
+
 	}
-
-
 	return true;
 }
 
-//This function palces the a piece on the board or returns an error message if an illgal column was chosen
-bool MakeMove(int** connectNBoard, int numRowsInBoard,  int playeID, int columnChosen)
+bool DisplayBoard (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard)
 {
 	int i = 0;
 	int j = 0;
-	
-	//Checks to see if the column chosen is within range of the board
-	//If it isn't, return an error message and prompt for another choice
-	if(columnChosen < 0 || columnChosen >= numRowsInBoard)
-	{
-		cout << endl << "Illegal Move" << endl;
-		cout << "That column is not on the board: try again" << endl;
-		cout << "Enter the column number where you want to put your piece" << endl;
-		cout << "Column number should be >=0 and <= " << numRowsInBoard - 1 << " ";
-		return false;
-	}
-	
-	//Checks to see if the column chosen is already filled
-	//If it is, then output a message and request another choice
-	if(connectNBoard[0][columnChosen] != 0)
-	{
-		cout << endl << "Illegal Move" << endl;
-		cout << "Column " << columnChosen << "is already completely full try again" << endl;
-		cout << "Enter column number where you want to put your piece" << endl;
-		cout << "Column number should be >= 0 and <= " << numRowsInBoard - 1 << " ";
-		return false;
-	}
-	
-	//Main for loop that will store the piece in the column of the board
-	for(i = (numRowsInBoard - 1); i >= 0; i--)
-	{
-		if(connectNBoard[i][columnChosen] == 0)
-		{
-			if(playeID == 1)
-			{
-				connectNBoard[i][columnChosen] = 1;
-				cout << "Red has moved";
-				return true;
-			}
-			else if(playeID == 2)
-			{
-				connectNBoard[i][columnChosen] = 2;
-				cout << "Black has moved";
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-			
-		}
-	}
-	
-	return false;
-}
 
-//This converts the integer array of connectNBoard to letters on the screen
-//Prints everything with a width of 3
-bool DisplayBoard( int** connectNBoard,  int numRowsInBoard)
-{
-	int i = 0;
-	int j = 0;
-	
-	//Sets an initial space for the top row of columns numbers
-	cout << endl << setw(3) << " ";
+	cout << endl << setw(3) << left << " ";
 
-	//Prints the top row of numbers of columns
-	for(i = 0; i < numRowsInBoard; i++)
-	{
-		cout << setw(3) << right << i;
-	}
-	cout << endl;
-	
-	//Prints in a square array each piece of the board, and sets everything else to 'o'
-	//Prints the row number at the beginning of each row
-	for(i = 0; i < numRowsInBoard; i++)
+	for (i = 0; i < numRowsInBoard; i++)
 	{
 		cout << setw(3) << left << i;
-		for(j = 0; j < numRowsInBoard; j++)
+	}
+	cout << endl;
+
+	for (i = 0; i < numRowsInBoard; i++)
+	{
+		cout << left << setw(3) << left << i;
+
+		for (j = 0; j < numRowsInBoard; j++)
 		{
-			if(j%(numRowsInBoard + 1) == numRowsInBoard)
+			if ( (j % (numRowsInBoard + 1)) == numRowsInBoard)
 			{
 				cout << endl;
 			}
-
-			if(connectNBoard[i][j] == 1)
+			if (connectNBoard[i][j] == 1)//R, 2 = B
 			{
-				cout << setw(3) << right << 'R';
+				cout << setw(3) << left << "R";
 			}
-			else if(connectNBoard[i][j] == 2)
+			else if (connectNBoard[i][j] == 2)
 			{
-				cout << setw(3) << right << 'B';
+				cout << setw(3) << left << "B"; 
 			}
-			else
+			else 
 			{
-				cout << setw(3) << right << 'o';
+				cout << setw(3) << left << "o";
 			}
 		}
 		cout << endl;
@@ -316,18 +197,67 @@ bool DisplayBoard( int** connectNBoard,  int numRowsInBoard)
 	return true;
 }
 
-//This function will check to see if the current move was a winning move
-bool CheckWinner(int** connectNBoard, int numRowsInBoard, int numConnect, int columnChosen, int playerID)
+bool MakeMove (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard, int player, int columnChosen)
+{
+	int i = 0;
+	int j = 0;
+
+	if (columnChosen < 0 || (columnChosen >= numRowsInBoard))
+	{
+		cout << endl << "Illegal Move" << endl;
+		cout << "That column is not on the board: try again" << endl << endl;
+		cout << "Enter the column number where you want to put your piece" << endl;
+		cout << "Column number should be >=0 and <= " << numRowsInBoard - 1 << " ";
+		return false;
+	}
+	
+	//checking to see if the column has been all filled
+	if(connectNBoard[0][columnChosen] != 0)
+	{
+		cout << endl << "Illegal Move" << endl;
+		cout << "Column " << columnChosen << " is already completely full try again" << endl << endl;
+		cout << "Enter column number where you want to put your piece" << endl;
+		cout << "Column number should be >= 0 and <= " << numRowsInBoard - 1 << " ";
+		return false;
+	}
+
+	for (i = (numRowsInBoard - 1); i >= 0; i--)
+	{
+		if (connectNBoard[i][columnChosen] == 0)
+		{
+			if (player == 1)
+			{
+				connectNBoard[i][columnChosen] = 1;
+				cout << "Red has moved";
+				return true;
+			}else if (player == 2)
+			{
+				connectNBoard[i][columnChosen] = 2;
+				cout << "Black has moved";
+				return true;
+			}
+			else 
+			{
+				return false;
+			}
+		}
+	}
+
+	return false;
+}
+
+
+bool CheckWinner (int connectNBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard, int numConnect, int columnChosen, int playerID)
 {
 	int afterCount = 0;
 	int beforeCount = 0;
 	int vertLOC = 0;
+
 	int i = 0;
 	int j = 0;
 	int k = 0;
-	
-	//If the column chosen is invalid, return false with an error message
-	if(columnChosen > (numRowsInBoard - 1) || columnChosen < 0)
+
+	if ((columnChosen < 0) || (columnChosen > numRowsInBoard -1))
 	{
 		cerr << "ERROR: invalid column chosen, cannot check for winner";
 		return false;
@@ -335,31 +265,46 @@ bool CheckWinner(int** connectNBoard, int numRowsInBoard, int numConnect, int co
 
 	//This for loop determines the row that the last piece was placed and stores it
 	//into vertLOC
-	for(i = 0; i < numRowsInBoard; i++)
+	for (i = 0; i < numRowsInBoard; i++)
 	{
-		if(connectNBoard[i][columnChosen] == 1 && playerID == 1)
+		if ((connectNBoard[i][columnChosen] == 1) && (playerID == 1))
 		{
 			vertLOC = i;
 			break;
 		}
-		else if(connectNBoard[i][columnChosen] == 2 && playerID == 2)
+		else if ((connectNBoard[i][columnChosen] == 2) && (playerID == 2))
 		{
 			vertLOC = i;
 			break;
 		}
-		else if(connectNBoard[i][columnChosen] != 0)
+		else if ((connectNBoard[i][columnChosen] != 0))
 		{
 			break;
-		}
-		else
+		}else
 		{
 			continue;
 		}
-		
 	}
 
 	//Counts the number of pieces of a certain player below the piece that was placed
 	for(i = vertLOC + 1; i < numRowsInBoard; i++)
+	{
+		if((connectNBoard[i][columnChosen] == 2) && (playerID == 2))
+		{
+			beforeCount += 1;
+		}
+		else if((connectNBoard[i][columnChosen] == 1) && (playerID == 1))
+		{
+			beforeCount += 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	//Vertical Checker above
+	for(i = vertLOC - 1; i >= 0; i--)
 	{
 		if(connectNBoard[i][columnChosen] == 2 && playerID == 2)
 		{
@@ -374,35 +319,16 @@ bool CheckWinner(int** connectNBoard, int numRowsInBoard, int numConnect, int co
 			break;
 		}
 	}
-	
-	
-   //Vertical Checker above
-    for(i = vertLOC - 1; i >= 0; i--)
-    {
-      if(connectNBoard[i][columnChosen] == 2 && playerID == 2)
-      {
-        beforeCount += 1;
-      }
-      else if(connectNBoard[i][columnChosen] == 1 && playerID == 1)
-      {
-        beforeCount += 1;
-      }
-      else
-      {
-        break;
-      }
-    }
 
 	//Adds the beforeCount and the actual location and check if it's greater than numConnect
-	if((beforeCount +  1) >= numConnect)
-	{
+	if ( (beforeCount + 1) >= numConnect )
+	{      
 		return true;
 	}
 	
 	//Resets the values of beforeCount to 0
 	beforeCount = 0;
 	afterCount = 0;
-
 
 	//Counts the pieces of the current player to the right of the most recently placed piece
 	//Adds that value to afterCount
@@ -421,7 +347,7 @@ bool CheckWinner(int** connectNBoard, int numRowsInBoard, int numConnect, int co
 			break;
 		}
 	}
-	
+
 	//Counts the pieces of the current player to the left of the most recently placed piece
 	//Adds that value to beforeCount
 	for(i = columnChosen - 1; i >= 0; i--)
@@ -439,14 +365,14 @@ bool CheckWinner(int** connectNBoard, int numRowsInBoard, int numConnect, int co
 			break;
 		}
 	}
-	
+
 	//Adds both afterCount and beforeCount and 1
 	//If this value is greater than numConnect, return true
 	if((beforeCount + afterCount + 1) >= numConnect)
 	{
 		return true;
 	}
-	
+
 	//Resets the values of k, beforeCount and afterCount
 	beforeCount = 0;
 	afterCount = 0;
@@ -461,12 +387,12 @@ bool CheckWinner(int** connectNBoard, int numRowsInBoard, int numConnect, int co
 		k += 1;
 		for(j = columnChosen + k; j < numRowsInBoard; j++)
 		{
-			if(connectNBoard[i][j] == 1 && playerID == 1)
+			if((connectNBoard[i][j] == 1) && (playerID == 1))
 			{
 				afterCount += 1;
 				break;
 			}
-			else if(connectNBoard[i][j] == 2 && playerID == 2)
+			else if((connectNBoard[i][j] == 2) && (playerID == 2))
 			{
 				afterCount += 1;
 				break;
@@ -477,7 +403,7 @@ bool CheckWinner(int** connectNBoard, int numRowsInBoard, int numConnect, int co
 			}
 		}
 	}
-	
+
 	//resets the value of k
 	k = 0;
 
@@ -505,14 +431,14 @@ bool CheckWinner(int** connectNBoard, int numRowsInBoard, int numConnect, int co
 			}
 		}
 	}
-	
+
 	//Compares the sum of beforeCount, afterCount and 1 to numConnect
 	//If it is greater or equal to numConnect, return true
 	if(beforeCount + afterCount + 1 >= numConnect)
 	{
 		return true;
 	}
-	
+
 	//Resets all values to 0
 	beforeCount = 0;
 	afterCount = 0;
@@ -542,7 +468,7 @@ bool CheckWinner(int** connectNBoard, int numRowsInBoard, int numConnect, int co
 			}
 		}
 	}
-	
+
 	k = 0;
 
 	//Counts the number of pieces of the given player, to the bottom left and adds them to beforeCount
@@ -569,12 +495,13 @@ bool CheckWinner(int** connectNBoard, int numRowsInBoard, int numConnect, int co
 			}
 		}
 	}
-	
+
 	//Compares the sum of beforeCount, afterCount, 1 to numConnect
 	//If they are greater than numConnect, return true
 	if((beforeCount + afterCount + 1) >= numConnect)
 	{
 		return true;
 	}
+
 	return false;
 }
